ffb311e6d81c051a87eed3fa4ae6ccf91cc372e6
ManOfJ <man.of.j@outlook.com>
2016-06-09 21:58:42 +0900

    Mod開発環境の設定ファイル更新に伴うファイル･ソースの修正

--- src/main/scala/com/manofj/minecraft/moj_fsmith/package.scala
+++ src/main/scala/com/manofj/minecraft/moj_fsmith/package.scala
@@ -5,10 +5,10 @@ package com.manofj.minecraft
   * 外部から参照可能な基本情報を定義しておく
   */
 package object moj_fsmith {
-  final val MOD_ID      = "@@MODID@@"
-  final val NAME        = "@@MODNAME@@"
-  final val VERSION     = "@@MODVERSION@@"
-  final val UPDATE_JSON = "@@UPDATEJSON@@"
+  final val MOD_ID      = "@modid@"
+  final val NAME        = "Furnasmith"
+  final val VERSION     = "@version@"
+  final val UPDATE_JSON = "http://manofj.com/minecraft/update?v=Furnasmith"
   final val LANGUAGE    = "scala"
   final val GUI_FACTORY = "com.manofj.minecraft.moj_fsmith.FurnasmithGuiFactory"
 }
